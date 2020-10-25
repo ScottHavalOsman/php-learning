2932a460a72f85ceb5cf894d37589324cb5077d7
Scott Osman <ScottOsman@protonmail.com>
2020-10-25 13:24:23 +0000

    while loops

--- index.php
+++ index.php
@@ -7,48 +7,24 @@
   <body>
 
     <?php
-    $friends = array("John", "Lucy", "Ivy");
-    echo 'Partner : ' . $friends[0] . '<br>';
-    $friends[3] = 'Steve';
-    foreach ($friends as $f) {
-      printf("Friend : %s<br>", $f);
+    $i = 0;
+
+    while ($i < 10) {
+      echo ++$i . ', ';
     }
-
-    $me_info = array('Name' => 'Derek', 'Street' => '123 Main Street');
-
-    foreach ($me_info as $k => $v) {
-      printf("%s : %s<br>", $k, $v);
-    }
-
-    $friends2 = array("Doug");
-
-    $friends = $friends + $friends2;
-
-    sort($friends);
-    rsort($friends);
-    arsort($me_info);
-    krsort($me_info);
-
-    $customers = array(array('Derek', '123 Main Street'),
-                       array('Sally', '122 Main Street'));
-
-    for($row = 0; $row < 2; $row++) {
-      for($col = 0; $col < 2; $col++) {
-        echo $customers[$row][$col] . ', ';
+    echo '<br>';
+    for ($i = 0; $i < 10; $i++) {
+      if(($i % 2) == 0) {
+        continue;
       }
-      echo  '<br>';
+      if($i == 7) break;
+      echo $i . ', ';
     }
-
-    $let_str = "A B C D";
-    $let_arr = explode(' ', $let_str);
-    foreach ($let_arr as $l) {
-      printf("Letter ; %s<br>", $l);
-    }
-    $let_str_2 = implode(' ', $let_arr);
-    echo "String : $let_str_2<br>";
-    printf("Key Exists : %d<br>",
-    array_key_exists('Name', $me_info));
-    printf("In Array : %d<br>", in_array('John', $friends));
+    echo '<br>';
+    $i = 0;
+    do{
+      echo "Do While : $i<br>";
+    } while($i > 0);
      ?>
 
   </body>
