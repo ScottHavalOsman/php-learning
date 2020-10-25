aa1376cd1eee9bae36a0ae7f6962287c1c044b9c
Scott Osman <ScottOsman@protonmail.com>
2020-10-25 12:25:10 +0000

    if statements

--- index.php
+++ index.php
@@ -6,5 +6,18 @@
   </head>
   <body>
 
+    <?php
+    $num_oranges = 4;
+    $num_bananas = 30;
+
+    if (($num_oranges > 4) && ($num_bananas > 30)) {
+      echo "25% Discount <br>";
+    } elseif (($num_oranges > 3) && ($num_bananas > 25)) {
+      echo "15% Discount <br>";
+    } else {
+      echo "No Discount";
+    }
+     ?>
+
   </body>
 </html>
