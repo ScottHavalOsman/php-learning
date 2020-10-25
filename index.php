53e7f0ee8b0a2154da471879ac61e88198ca4d91
Scott Osman <ScottOsman@protonmail.com>
2020-10-25 12:51:44 +0000

    index

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
     $rand_str = trim($rand_str);
     printf("Upper: %s<br>", strtoupper($rand_str));
     printf("Lower: %s<br>", strtolower($rand_str));
+    printf("Upper First Character: %s<br>", ucfirst($rand_str));
+    printf("1st 6 Characters : %s<br>", substr($rand_str, 0, 6));
+
      ?>
 
   </body>
