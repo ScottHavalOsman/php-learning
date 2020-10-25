5c7f496ed2417de22e4a532b1c340a7038ebe101
Scott Osman <ScottOsman@protonmail.com>
2020-10-25 13:16:52 +0000

    working with sorts and array methods

--- index.php
+++ index.php
@@ -20,6 +20,35 @@
       printf("%s : %s<br>", $k, $v);
     }
 
+    $friends2 = array("Doug");
+
+    $friends = $friends + $friends2;
+
+    sort($friends);
+    rsort($friends);
+    arsort($me_info);
+    krsort($me_info);
+
+    $customers = array(array('Derek', '123 Main Street'),
+                       array('Sally', '122 Main Street'));
+
+    for($row = 0; $row < 2; $row++) {
+      for($col = 0; $col < 2; $col++) {
+        echo $customers[$row][$col] . ', ';
+      }
+      echo  '<br>';
+    }
+
+    $let_str = "A B C D";
+    $let_arr = explode(' ', $let_str);
+    foreach ($let_arr as $l) {
+      printf("Letter ; %s<br>", $l);
+    }
+    $let_str_2 = implode(' ', $let_arr);
+    echo "String : $let_str_2<br>";
+    printf("Key Exists : %d<br>",
+    array_key_exists('Name', $me_info));
+    printf("In Array : %d<br>", in_array('John', $friends));
      ?>
 
   </body>
